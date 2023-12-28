024db0d4c3eba5acd919eb904e59b7fb15ac2569
Tim <Azacar02@gmail.com>
2023-12-28 15:41:07 -0500

    Updating with changes from lesson

--- src/intersection/index.js
+++ src/intersection/index.js
@@ -1,3 +1,20 @@
-function intersection(a, b) {}
+function intersection(a, b) {
+    let temp = new Map();
+    let tempSet = new Set();
+
+
+    for (let i = 0; i < a.length; i++) {
+        temp.set(a[i]);
+    }
+
+    for (let i = 0; i < b.length; i++) {
+        if (temp.has(b[i])) {
+            tempSet.add(b[i]);
+        }
+    }
+
+    let result = [...tempSet];
+    return result;
+}
 
 module.exports = intersection;
--- src/sumPairs/index.js
+++ src/sumPairs/index.js
@@ -3,6 +3,29 @@
  * @param {Array} A an array of numbers
  * @param {Number} N an integer value
  */
-function sumPairs(A, N) {}
+function sumPairs(A, N) {
+  if (Array.isArray(A)) {
+    let numbers = new Map();
+    let solution = new Map();
+
+    for (let i = 0; i < A.length; i++) {
+      numbers.set(A[i], A[i]);
+    }
+
+    for (let i = 0; i < A.length; i++) {
+      let diff = N - A[i];
+      if (diff !== A[i]) {
+        if (numbers.has(diff)) {
+          solution.set(Math.min(A[i], diff), Math.max(A[i], diff));
+        }
+      }
+    }
+
+    let result = [...solution];
+    return result;
+  }
+
+  return [];
+}
 
 module.exports = sumPairs;
